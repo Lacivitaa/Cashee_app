8ac91939441601ff8cacba71dcf70e4355f4b100
lacivita <61033918+Lacivitaa@users.noreply.github.com>
2020-08-04 13:56:05 -0300

    Initial commit

---
+++ cashee_money_helper/src/cashee_money_helper/Principal.java
@@ -0,0 +1,16 @@
+package cashee_money_helper;
+
+public class Principal {
+	public static void main(String[] args) {
+		Funcionalidades func = new Funcionalidades();
+		if (func.menuPrincipal() == 1) {
+			
+		}
+		else if(func.menuPrincipal() == 2) {
+			
+		}
+		else if(func.menuPrincipal() == 3) {
+			
+		}
+	}	
+}
